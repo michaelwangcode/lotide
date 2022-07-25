6b6d795f06f86cbcb53597e71bef5368fe78c356
Michael Wang <michaelwangxv@gmail.com>
2022-07-25 21:23:36 +0000

    Create assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log('✅ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('❌ Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+assertEqual("Hello", "Hello");
+assertEqual("", " ");
+assertEqual("", "");
+assertEqual(9, 9);
+assertEqual("10", 10);
