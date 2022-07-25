0cdad46aab68ad3b71addf9fb72414ecabd5591f
Michael Wang <michaelwangxv@gmail.com>
2022-07-25 21:40:24 +0000

    Add head function

---
+++ head.js
@@ -0,0 +1,20 @@
+const head = function(array) {
+  return array[0];
+};
+
+
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+
+assertEqual(head([1]), 1);
+assertEqual(head([[1]]), 1);
+assertEqual(head([]), undefined);
