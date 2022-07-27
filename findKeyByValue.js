a62b5c2f4458414573a65301a32efe81645bd0b2
Michael Wang <michaelwangxv@gmail.com>
2022-07-27 20:48:22 +0000

    Added findKeyByValue

---
+++ findKeyByValue.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+// Find the key of an object given a value
+const findKeyByValue = function(object, value) {
+
+  // Iterate through the keys in the object
+  for (let key in object) {
+    // If the value is found, return the key
+    if (object[key] === value) {
+      return key;
+    }
+  }
+
+  return undefined;
+};
+
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
