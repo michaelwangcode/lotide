1a547f3e99daf5a57d0b8030610673f40160ff99
Michael Wang <michaelwangxv@gmail.com>
2022-07-28 22:54:24 +0000

    Add findKey

---
+++ findKey.js
@@ -0,0 +1,45 @@
+// Check if two values are equal
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+  
+
+// This function takes an object and callback function
+// It returns the first key that returns a value of false
+const findKey = function(object, callback) {
+    
+    for (let key in object) {
+      if (callback(object[key])) {
+        return key;
+      }
+    }
+
+}
+
+
+// Test statements
+let test = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2) // => "noma"
+
+let test2 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 3) // => "noma"
+
+
+assertEqual(test, "noma");
+assertEqual(test2, "Akaleri");
