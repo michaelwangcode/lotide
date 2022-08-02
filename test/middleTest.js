4ea6ee86bf0a5ef86dd9be8e2d613a22bee08af0
Michael Wang <michaelwangxv@gmail.com>
2022-08-02 01:32:49 +0000

    Add middleTest testing

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,3 +1,6 @@
+// Import Chai
+const assert = require('chai').assert;
+
 // Import the assertArraysEqual function
 const assertArraysEqual = require('../assertArraysEqual');
 
@@ -6,11 +9,30 @@ const middle = require('../middle');
 
 
 // Test code for middle.js
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
+describe("#middle", () => {
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  it("returns [] for [[1]]", () => {
+    assert.deepEqual(middle([1]), []);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]);
+  it("returns [] for [[1, 2]]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+
+  it("returns [2] for [[1, 2, 3]]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+  it("returns [3] for [[1, 2, 3, 4, 5]]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+  it("returns [2, 3] for [[1, 2, 3, 4]]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it("returns [3] for [[1, 2, 3, 4, 5, 6]]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+
+});
