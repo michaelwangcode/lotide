0534abb4e2eb91f6a0970edd94a523be74451cfb
Michael Wang <michaelwangxv@gmail.com>
2022-08-02 00:12:49 +0000

    Add Chai assertions

--- test/headTest.js
+++ test/headTest.js
@@ -1,14 +1,35 @@
-// Import the assertEqual function
-const assertEqual = require('../assertEqual');
+// Import Chai
+const assert = require('chai').assert;
 
 // Import the head function
-const head = require('../head');
+const head   = require('../head');
 
 
 // Test code for head.js
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+describe("#head", () => {
 
-assertEqual(head([1]), 1);
-assertEqual(head([[1]]), 1);
-assertEqual(head([]), undefined);
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5'); 
+  });
+
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(['Hello', 'Lighthouse', 'Labs']), 'Hello');
+  });
+
+  it("returns [1] for [[1]]", () => {
+    assert.equal(head([[1]]), 1);
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+
+});
